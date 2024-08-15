aa381f1c5bc924f0f2cda3e6e19a0917bbd8a92c
musicode <musicode@qq.com>
2024-08-15 09:46:19 +0800

    bump 0.1.3

--- library/src/main/java/com/github/herokotlin/cameraview/CameraView.kt
+++ library/src/main/java/com/github/herokotlin/cameraview/CameraView.kt
@@ -6,7 +6,6 @@ import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import android.util.AttributeSet
 import android.util.Base64
